18bcede29ed8910996ea621b1e55c9e4e6b86663
MuhammedZeki <muhammedzekiylmz5353@gmail.com>
2025-08-11 15:37:47 +0300

    first

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mongoose = require("mongoose");
+const { config } = require("dotenv");
+app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
+config();
+const connectToDb = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Connect To Db");
+  } catch (error) {
+    console.log("Error");
+  }
+};
+
+app.get("/message", (req, res) => {
+  res.json({ message: "Backend is running!" });
+});
+
+app.listen(5000, () => {
+  connectToDb();
+  console.log("Server is running on port 5000");
+});
